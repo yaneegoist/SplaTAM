32835296bec604480db068c7403c7ef6dc08be50
yaneegoist <iamobsessedwithtiktok@gmail.com>
2026-02-21 21:45:50 +0300

    bug fix

--- utils/submap.py
+++ utils/submap.py
@@ -4,6 +4,53 @@ from utils.slam_external import build_rotation
 from utils.slam_helpers import transform_to_frame, transformed_params2rendervar, transformed_params2depthplussilhouette
 from utils.keyframe_selection import keyframe_selection_overlap
 
+def get_pointcloud(color, depth, intrinsics, w2c, transform_pts=True, 
+                   mask=None, compute_mean_sq_dist=False, mean_sq_dist_method="projective"):
+    width, height = color.shape[2], color.shape[1]
+    CX = intrinsics[0][2]
+    CY = intrinsics[1][2]
+    FX = intrinsics[0][0]
+    FY = intrinsics[1][1]
+
+    x_grid, y_grid = torch.meshgrid(torch.arange(width).cuda().float(), 
+                                    torch.arange(height).cuda().float(),
+                                    indexing='xy')
+    xx = (x_grid - CX)/FX
+    yy = (y_grid - CY)/FY
+    xx = xx.reshape(-1)
+    yy = yy.reshape(-1)
+    depth_z = depth[0].reshape(-1)
+
+    pts_cam = torch.stack((xx * depth_z, yy * depth_z, depth_z), dim=-1)
+    if transform_pts:
+        pix_ones = torch.ones(height * width, 1).cuda().float()
+        pts4 = torch.cat((pts_cam, pix_ones), dim=1)
+        c2w = torch.inverse(w2c)
+        pts = (c2w @ pts4.T).T[:, :3]
+    else:
+        pts = pts_cam
+
+    if compute_mean_sq_dist:
+        if mean_sq_dist_method == "projective":
+            scale_gaussian = depth_z / ((FX + FY)/2)
+            mean3_sq_dist = scale_gaussian**2
+        else:
+            raise ValueError(f"Unknown mean_sq_dist_method {mean_sq_dist_method}")
+    
+    cols = torch.permute(color, (1, 2, 0)).reshape(-1, 3)
+    point_cld = torch.cat((pts, cols), -1)
+
+    if mask is not None:
+        point_cld = point_cld[mask]
+        if compute_mean_sq_dist:
+            mean3_sq_dist = mean3_sq_dist[mask]
+
+    if compute_mean_sq_dist:
+        return point_cld, mean3_sq_dist
+    else:
+        return point_cld
+
+
 class Submap:
     def __init__(self, submap_id, first_frame_data, config, num_frames_global):
         self.id = submap_id
